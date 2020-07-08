7c17538bb922fcaf2c3c141de787e90ee1f80dda
Ruchika <ruchikaster@gmail.com>
2020-07-08 12:24:53 +0200

    adding first page

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Directory from '../../compoents/directory-menu/directory-menu.component';
+
+import './homepage.component.styles.scss';
+
+const HomePage = () => (
+    <div className = 'homepage'>
+        <Directory/>
+    </div>
+
+);
+
+export default HomePage;
